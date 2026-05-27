ae874e1b385defbbb74c3e27a64f4a0405093f95
Joshua Brooks <joshua@appfusion.com.au>
2026-05-28 09:20:51 +1000

    Add egg hatching state

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include "sprites/egg/egg.h"
-#include "sprites/title/title.h"  
+#include "sprites/title/title.h"
 #include "sprites/credits/credits.h"
+#include "sprites/tia/tia.h"
+#include "sprites/speech/speech.h"
 #include "buttons.h"
 
 
@@ -11,10 +13,11 @@
 
 U8G2_SH1106_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, SCREEN_SCL_PIN, SCREEN_SDA_PIN, U8X8_PIN_NONE);
 
-enum class GameState { TITLE, CREDIT, EGG, ALIVE, DIED };
+enum class GameState { TITLE, CREDIT, EGG, HATCHING, ALIVE, DIED };
 
 GameState gameState = GameState::TITLE;
 unsigned long creditEnteredAt = 0;
+unsigned long hatchStartedAt  = 0;
 
 void drawTitle() {
   u8g2.drawXBMP(0, 0, 128, 30, title_f0);
@@ -23,7 +26,25 @@ void drawTitle() {
 }
 
 void drawEgg() {
-  u8g2.drawXBMP(48, 16, EGG_WIDTH, EGG_HEIGHT, egg_f0);
+  // Wobble pattern: mostly centred, occasional left/right nudges
+  static const int8_t shakeX[] = { 0, -2, 0, 2, 0, -1, 0, 1 };
+  int8_t ox = shakeX[(millis() / 120) % 8];
+  uint8_t frame = (millis() / 500) % EGG_FRAMES;
+  u8g2.drawXBMP(48 + ox, 16, EGG_WIDTH, EGG_HEIGHT, egg_frames[frame]);
+}
+
+void drawHatching() {
+  // Step through Tia frames 0→1→2, 2s each; clamp at frame 2
+  unsigned long elapsed = millis() - hatchStartedAt;
+  uint8_t frame = (uint8_t)(elapsed / 2000);
+  if (frame > 2) frame = 2;
+  u8g2.drawXBMP(16, 16, TIA_WIDTH, TIA_HEIGHT, tia_frames[frame]);
+}
+
+void drawAlive() {
+  // Tia frame 2 on the left, speech bubble to her right
+  u8g2.drawXBMP(16, 16, TIA_WIDTH, TIA_HEIGHT, tia_frames[2]);
+  u8g2.drawXBMP(16 + TIA_WIDTH + 4, 8, SPEECH_WIDTH, SPEECH_HEIGHT, speech_f0);
 }
 
 void drawCredits() {
@@ -51,7 +72,16 @@ void loop() {
     case GameState::CREDIT:
       if (millis() - creditEnteredAt >= 5000) gameState = GameState::EGG;
       break;
-    case GameState::EGG:   break;
+    case GameState::EGG:
+      if (anyButton) {
+        Serial.println("time for egg to hatch");
+        gameState = GameState::HATCHING;
+        hatchStartedAt = millis();
+      }
+      break;
+    case GameState::HATCHING:
+      if (millis() - hatchStartedAt >= 6000) gameState = GameState::ALIVE;
+      break;
     case GameState::ALIVE: break;
     case GameState::DIED:  break;
   }
@@ -62,8 +92,9 @@ void loop() {
   switch (gameState) {
     case GameState::TITLE:  drawTitle();   break;
     case GameState::CREDIT: drawCredits(); break;
-    case GameState::EGG:    drawEgg();     break;
-    case GameState::ALIVE: break;
+    case GameState::EGG:      drawEgg();      break;
+    case GameState::HATCHING: drawHatching(); break;
+    case GameState::ALIVE:    drawAlive();    break;
     case GameState::DIED:  break;
   }
 
